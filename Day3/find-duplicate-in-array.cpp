bc0e2c7093f2ca644158ee3f79bbf16cac8318ec
Vanshul Choudhary <75979288+Cvanshulll@users.noreply.github.com>
2023-06-08 23:45:58 +0530

    Create find-duplicate-in-array.cpp

---
+++ Day3/find-duplicate-in-array.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+int findDuplicate(vector<int> &arr, int n){
+	unordered_map<int,int>mp;
+	for(auto it:arr){
+		mp[it]++;
+	}
+	for(auto it:mp){
+		if(it.second>1){
+			return it.first;
+		}
+	}
+}
